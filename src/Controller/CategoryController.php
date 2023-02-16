5fe1d253470dc1f5dfc66a92bc176bdb8708e31e
vothuc2k3 <vothuc2k3@gmail.com>
2023-02-16 17:18:08 +0700

    categorymanager

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/category")
@@ -40,9 +39,34 @@ class CategoryController extends AbstractController
     {
         $c = new Category();
         $form = $this->createForm(CategoryType::class, $c);
+
         $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->repo->add($c, true);
+            return $this->redirectToRoute('category_show', [], Response::HTTP_SEE_OTHER);
+        }
         return $this->render("category/form.html.twig", [
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/delete/{id}",name="category_delete",requirements={"id"="\d+"})
+     */
+
+     public function deleteAction(Request $request, Category $c): Response
+     {
+         $this->repo->remove($c, true);
+         return $this->redirectToRoute('category_show', [], Response::HTTP_SEE_OTHER);
+     }
+
+         /**
+     * @Route("/edit/{id}",name="category_edit",requirements={"id"="\d+"})
+     */
+
+     public function editAction(Request $request, Category $c): Response
+     {
+         $this->repo->remove($c, true);
+         return $this->redirectToRoute('product_show', [], Response::HTTP_SEE_OTHER);
+     }
 }
